8945e909da3e7e4d23c4884f1222da749991aeea
Gurlabe Dhaliwal <GurlabeD@Ramans-MacBook-Air.local>
2018-11-15 14:22:46 -0800

    Beginning of React shopping cart, initial header footer and cart item components initialiZed

---
+++ src/components/CartFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CartFooter() {
+    return (
+    <div>
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">&copy; 2018</a>
+        </nav>
+    </div>
+    )
+}
+export default CartFooter  
---
+++ src/components/CartItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CartItems() {
+    return (
+    <div>
+        <div className="container">
+            <h1>COSAS</h1>
+        </div>
+    </div>
+    )
+}
+
+export default CartItems
